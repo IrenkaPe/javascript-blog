f3df0835cf57d47c1e983590f43d365fc541272f
Klaudia Perszke <klaudia.perszke@gmail.com>
2025-07-30 22:47:23 +0200

    add class active to the correct article

--- js/script.js
+++ js/script.js
@@ -31,8 +31,10 @@ const articleSelector = clickedElement.getAttribute('href');
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
- const targetArticle = document.querySelector('href')
+const targetArticle = document.querySelector(articleSelector);
   /* add class 'active' to the correct article */
+
+targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
